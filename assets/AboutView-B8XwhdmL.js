a9da4f0159fd2161e48cfc495743be657310c5ce
Juan Pablo Pérez <juan.perez@lorspi.com>
2026-09-10 12:53:34 -0500

    deploy: publish site from fuente@d818524

--- assets/AboutView-B8XwhdmL.js
+++ assets/AboutView-B8XwhdmL.js
@@ -1,4 +1,4 @@
-import{j as e}from"./markdown-BbsDIvqd.js";import{A as r}from"./arrow-left-rIk7E4Kp.js";import{c as t}from"./index-BRU1v1xC.js";import"./react-_cmU9Ofy.js";/**
+import{j as e}from"./markdown-BbsDIvqd.js";import{A as r}from"./arrow-left-B2erIhVz.js";import{c as t}from"./index-BJ1FwWqy.js";import"./react-_cmU9Ofy.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/ArchiveView-DloUpPpM.js
+++ assets/ArchiveView-DloUpPpM.js
@@ -1,4 +1,4 @@
-import{j as e,a as m}from"./markdown-BbsDIvqd.js";import{c as b,X as L,C as M}from"./index-BRU1v1xC.js";import{L as S}from"./LetterList-DglYMOkY.js";import"./react-_cmU9Ofy.js";import"./CategoryBadge-CdwhsufW.js";/**
+import{j as e,a as m}from"./markdown-BbsDIvqd.js";import{c as b,X as L,C as M}from"./index-BJ1FwWqy.js";import{L as S}from"./LetterList-Bgc0z18X.js";import"./react-_cmU9Ofy.js";import"./CategoryBadge-CdwhsufW.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
